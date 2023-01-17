77aef2c1210c8e2df8cccde11ff71d6231d8894b
Aleksey (Faumaray) Selihov <faumaray@gmail.com>
2023-01-18 01:43:59 +0400

    trying wine wayland builds fix

--- pkgs/Wine/default.nix
+++ pkgs/Wine/default.nix
@@ -1,7 +1,7 @@
 {pkgs}:
 (pkgs.wineWowPackages.waylandFull.overrideAttrs    (oldAttrs: {
         repo = "wine";
-        rev = "wayland";
+        rev = "0a941afbe6a06009b0859f745e505b51466c51fa";
         version = "8.0-rc4";
         hash = "sha256-KpmuvjOHccQWRs7z7Nxp6gX2+ZF74rdeuWjiLmtOC8g=";
 }))
