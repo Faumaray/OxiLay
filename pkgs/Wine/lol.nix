93adff2cd7b870d44c6253ddcfd93e20dab8cb32
Aleksey (Faumaray) Selihov <faumaray@gmail.com>
2023-01-18 13:27:42 +0400

    trying wine wayland builds fix

--- pkgs/Wine/lol.nix
+++ pkgs/Wine/lol.nix
@@ -1,11 +1,8 @@
-{pkgs}:
+{pkgs, lib, callPackage}:
 (pkgs.wineWowPackages.waylandFull.overrideAttrs (oldAttrs: {
-        repo = "wine";
-        rev = "0a941afbe6a06009b0859f745e505b51466c51fa";
+        src = lib.getAttr "source" (callPackage ./source.nix {});       # Due strange clone
         version = "8.0-rc4";
-    hash = "sha256-KpmuvjOHccQWRs7z7Nxp6gX2+ZF74rdeuWjiLmtOC8g=";
-        # Due strange clone
         patches = [
-        ./full.patch
+        ./lol.patch
         ];
-    }))
+}))
