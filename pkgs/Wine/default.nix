1df696e1dce6ae4bf73017231b3e943c74eed0b4
Aleksey (Faumaray) Selihov <faumaray@gmail.com>
2023-01-18 13:35:18 +0400

    swap wayland build source without lol patch

--- pkgs/Wine/default.nix
+++ pkgs/Wine/default.nix
@@ -1,8 +1,5 @@
-{pkgs}:
-(pkgs.wineWowPackages.waylandFull.overrideAttrs    (oldAttrs: {
-        repo = "wine";
-        rev = "0a941afbe6a06009b0859f745e505b51466c51fa";
+{pkgs, lib, callPackage}:
+(pkgs.wineWowPackages.waylandFull.overrideAttrs (oldAttrs: {
+        src = lib.getAttr "source" (callPackage ./source.nix {});       # Due strange clone
         version = "8.0-rc4";
-        hash = "sha256-KpmuvjOHccQWRs7z7Nxp6gX2+ZF74rdeuWjiLmtOC8g=";
-}))
-
+  }))
