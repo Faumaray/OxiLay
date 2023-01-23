51ba51c96c16fef52aec23a65f3ac739d1f841a2
Aleksey (Faumaray) Selihov <faumaray@gmail.com>
2023-01-23 11:54:56 +0400

    Change league of legends patch to multiple

--- Packages/Wine/lol.nix
+++ Packages/Wine/lol.nix
@@ -6,6 +6,10 @@
   src = lib.getAttr "source" (callPackage ./source.nix {}); # Due strange clone
   version = "8.0-rc4";
   patches = [
-    ./lol.patch
+    ./league_of_legends_patches/0002-LoL-6.17+-syscall-fix.patch
+    ./league_of_legends_patches/0003-LoL-abi.vsyscall32-alternative_patch_by_using_a_fake_cs_segment.patch
+    ./league_of_legends_patches/0004-LoL-broken-client-update-fix.patch
+    ./league_of_legends_patches/0005-LoL-client-slow-start-fix.patch
+    ./league_of_legends_patches/0006-LoL-abi-vsyscall32-disable-vDSO.patch
   ];
 }))
