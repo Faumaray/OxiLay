f92f9349df3b5eaf54f48750ce95d19302ad545a
Aleksey Selihov <faumaray@gmail.com>
2023-01-26 17:40:36 +0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -18,11 +18,11 @@
         pkgs = prev;
       };
 
-    packages = forAllSystems (system:
-      self.overlays.default null (import nixpkgs {
+    packages = {forAllSystems (system:
+      default.${system} = self.overlays.default null (import nixpkgs {
         inherit system;
         config.allowUnfree = true;
-      }));
+      }));};
 
     homeManagerModules.default = import ./HMModules self;
 
